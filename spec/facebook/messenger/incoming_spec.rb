f3da8c8fabe169175e84e6d39af9feae159f7e35
nico_lrx <nicolas.leroux@sciencespo.fr>
2017-07-12 21:56:27 +0200

    Revert "Fix metadata issue for echo payload"

--- spec/facebook/messenger/incoming_spec.rb
+++ spec/facebook/messenger/incoming_spec.rb
@@ -55,8 +55,7 @@ describe Facebook::Messenger::Incoming do
             'is_echo' => true,
             'mid' => 'mid.1457764197618:41d102a3e1ae206a38',
             'seq' => 73,
-            'text' => 'Hello, bot!',
-            'metadata' => 'metadata'
+            'text' => 'Hello, bot!'
           }
         }
       end
